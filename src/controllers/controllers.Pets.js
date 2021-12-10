675dcbc9e9efa4bad30e0fc157def1ff294ee468
Jaziel <jaziel.isma@outlook.es>
2021-12-10 13:43:30 -0300

    update pet for id added

--- src/controllers/controllers.Pets.js
+++ src/controllers/controllers.Pets.js
@@ -18,6 +18,14 @@ Ctrls.getPetForId = async (req, res) => {
     res.json({pet: petFound})
 } 
 
+// update pet
+
+Ctrls.updatePetForId = async (req, res) => {
+    const id = req.params.petID;
+    const petFound = await Pets.findByIdAndUpdate(id, req.boy,{new: true});
+
+    res.json({pet: petFound, msg: 'La mascota se actualizó correctamente'})
+}
 
 
 
